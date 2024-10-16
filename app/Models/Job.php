56483d5b663ffeeed6d17a1e31bcdfe25d03bd44
DerickMghanga <derickmghanga3329@gmail.com>
2024-10-16 22:41:08 +0300

    namespaces and models

---
+++ app/Models/Job.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Job
+{
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$150000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$100000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$60000'
+            ]
+        ];
+    }
+
+    public static function find(int $id)
+    {
+        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
+
+        if (! $job) {   //incase page doesn't exist since no product has that ID
+            abort(404);
+        }
+    }
+}
