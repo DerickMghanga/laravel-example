a8cee141946164bcedd35203b6b4ccc8569fd140
DerickMghanga <derickmghanga3329@gmail.com>
2024-10-25 04:35:35 +0300

    Eloquent for MySQL DB access

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
-{
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$150000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$100000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$60000'
-            ]
-        ];
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job) {   //incase page doesn't exist since no product has that ID
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
